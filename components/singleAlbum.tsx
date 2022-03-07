2c94f2c3752a5d55f59552b74a78569d6f4238b9
pzworks <piotrziolkowski92@gmail.com>
2022-03-07 22:19:54 +0100

    click on edit of album on list directs to edit form

--- components/singleAlbum.tsx
+++ components/singleAlbum.tsx
@@ -1,13 +1,18 @@
 import {Pressable, Text, View} from "react-native";
 import {albumStyles} from "../styles/albumStyles";
+import {Entypo} from "@expo/vector-icons";
+import {mainColor} from "../styles/common";
 
 const SingleAlbum = props => {
   const { album, navigation } = props
 
   return (
     <View style={albumStyles.album}>
+      <Pressable onPress={() => navigation.navigate('Album form', {album})}>
+        <Entypo name={'edit'} size={24} color={mainColor} style={{textAlign: 'right'}}/>
+      </Pressable>
       <Text style={albumStyles.albumTitle}>ID: {album.id} </Text>
-      <Pressable onPress={() => navigation.navigate('Albums details', {album: album})}>
+      <Pressable onPress={() => navigation.navigate('Albums details', {album})}>
         <Text style={albumStyles.albumTitle}>Title: {album.title}</Text>
       </Pressable>
     </View>
